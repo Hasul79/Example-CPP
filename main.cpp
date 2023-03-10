7d974408390da575fe6fc6646f54faceca8bb2c1
Hasul79 <hatuk2@mail.ru>
2023-03-10 23:38:25 +0400

    first commit

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+   // cout << 1 << endl << 2 << endl << 3 << endl << 4 << endl << 5 << endl;
+   // cout << 5 + 1 << endl << 5 * 2 << endl  << (5 + 1) / 3 << endl;
+   // cout << 5 % 2 << endl << 5 - 2 << endl ;
+   // cout << "Hello Armenia" << endl ;
+
+   int x = 7;
+   x = 9;
+   int y = x + 5;
+   int z = y / 2;
+   cout << z << endl ;
+  
+   return 0;
+}
