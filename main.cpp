389f2d4886ebaeb3d2163139a7946ad8fe7fb07c
Hasul79 <hatuk2@mail.ru>
2023-03-11 22:25:02 +0400

    third

--- main.cpp
+++ main.cpp
@@ -7,11 +7,15 @@ int main() {
    // cout << 5 % 2 << endl << 5 - 2 << endl ;
    // cout << "Hello Armenia" << endl ;
 
-   int x = 7;
-   x = 9;
-   int y = x + 5;
-   int z = y / 2;
-   cout << z << endl ;
+   // int x = 7;
+   // x = 9;
+   // int y = x + 5;
+   // int z = y / 2;
+   // cout << z << endl ;
+
+   int x;
+   cin >> x;
+   cout << x + 5 << endl;
   
    return 0;
 }
